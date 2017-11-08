863f2b51c8e00e733282b001383d91a3e6fe8fbd
vpWithWpml1 <veljko94pesic@gmail.com>
2017-11-08 09:41:09 +0100

    [VP] Created 2 posts
    VP-Action: post/create/BD5F1EF9CEAD4C369C7260DADE4440D6
    VP-Post-Title: xzy
    VP-Post-Type: page
    VP-Post-Format: page
    
    VP-Action: icl_translation/create/9473FC14DE0245F0B78B6FA02ED6F89F
    
    VP-Action: icl_translation_status/create/6EB9DDA9B7E64A11AB58E730745ADEBE
    
    VP-Action: icl_translate_job/create/FFA89A1B3DBC487FA63A05AF1A40B61E
    
    VP-Action: icl_translate/create/58DB77F896204D6B8B4C11804D3FDBE9
    
    VP-Action: icl_translate/create/C46BDF91C7834F8E9A4CF9F297FFD59A
    
    VP-Action: icl_translate/create/76133B0492C34A3D9DE3C443C80AEFB8
    
    VP-Action: icl_translate/create/FAFF433EA9A440879CFD7E67623060B5
    
    VP-Action: post/create/2241507269B449DBAC5A48A6AD48A68C
    VP-Post-Title: download
    VP-Post-Type: attachment
    VP-Post-Format: attachment
    
    VP-Action: icl_translation/create/7F75E5B617B04DE5B3D5A0DBD601852B
    
    VP-Action: icl_translation_status/create/55AA6B90E32A463D81269AF4430BEAFB
    
    VP-Action: icl_translation_status/update/6EB9DDA9B7E64A11AB58E730745ADEBE
    
    VP-Action: icl_translate/update/C46BDF91C7834F8E9A4CF9F297FFD59A
    
    VP-Action: icl_translate/update/76133B0492C34A3D9DE3C443C80AEFB8
    
    VP-Action: icl_translate_job/update/FFA89A1B3DBC487FA63A05AF1A40B61E
    
    VP-Action: icl_translate/update/58DB77F896204D6B8B4C11804D3FDBE9
    
    VP-Action: icl_translate/update/FAFF433EA9A440879CFD7E67623060B5
    
    VP-Action: icl_translation_status/update/55AA6B90E32A463D81269AF4430BEAFB
    
    VP-Action: postmeta/create/201FCDCC447747899B5E38CA8A450CCB
    VP-Post-Id: BD5F1EF9CEAD4C369C7260DADE4440D6
    VP-PostMeta-Key: _wpml_media_duplicate
    VP-Post-Type: page
    VP-Post-Title: xzy
    
    VP-Action: postmeta/create/52F1E82479C445C09699950C107BDEB7
    VP-Post-Id: BD5F1EF9CEAD4C369C7260DADE4440D6
    VP-PostMeta-Key: _wpml_media_featured
    VP-Post-Type: page
    VP-Post-Title: xzy
    
    VP-Action: postmeta/create/C8F958B7E585443793C8D71403B8384D
    VP-Post-Id: BD5F1EF9CEAD4C369C7260DADE4440D6
    VP-PostMeta-Key: _wp_page_template
    VP-Post-Type: page
    VP-Post-Title: xzy
    
    VP-Action: postmeta/create/58A64A3C04274AAB89D100D112F5A6D4
    VP-Post-Id: 2241507269B449DBAC5A48A6AD48A68C
    VP-PostMeta-Key: _wp_attachment_metadata
    VP-Post-Type: attachment
    VP-Post-Title: download
    
    VP-Action: postmeta/create/7098029DEEB64903B1C501F10D949360
    VP-Post-Id: 2241507269B449DBAC5A48A6AD48A68C
    VP-PostMeta-Key: wpml_media_processed
    VP-Post-Type: attachment
    VP-Post-Title: download
    
    VP-Action: postmeta/create/FBDC2CFFC1344782870DEDC7B1743FD5
    VP-Post-Id: 2241507269B449DBAC5A48A6AD48A68C
    VP-PostMeta-Key: _wp_attached_file
    VP-Post-Type: attachment
    VP-Post-Title: download
    
    X-VP-Version: 4.0-beta
    X-VP-Environment: default

---
+++ wp-content/uploads/cache/wpml/twig/64/64f0a6eac0e3fcdb71f4b9e14133205ea771d091914401f3e8d85b5da2ef05ba.php
@@ -0,0 +1,157 @@
+<?php
+
+/* template.twig */
+class __TwigTemplate_9d6e4ae1566bf9c4f320236b00c91bc2d25cc3a5fd509c62585ae87945fac6e0 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        $context["css_classes_flag"] = trim(("wpml-ls-flag " . $this->getAttribute(($context["backward_compatibility"] ?? null), "css_classes_flag", array())));
+        // line 2
+        $context["css_classes_native"] = trim(("wpml-ls-native " . $this->getAttribute(($context["backward_compatibility"] ?? null), "css_classes_native", array())));
+        // line 3
+        $context["css_classes_display"] = trim(("wpml-ls-display " . $this->getAttribute(($context["backward_compatibility"] ?? null), "css_classes_display", array())));
+        // line 4
+        $context["css_classes_bracket"] = trim(("wpml-ls-bracket " . $this->getAttribute(($context["backward_compatibility"] ?? null), "css_classes_bracket", array())));
+        // line 5
+        echo "
+<div class=\"";
+        // line 6
+        echo twig_escape_filter($this->env, ($context["css_classes"] ?? null), "html", null, true);
+        echo " wpml-ls-legacy-list-vertical\"";
+        if ($this->getAttribute(($context["backward_compatibility"] ?? null), "css_id", array())) {
+            echo " id=\"";
+            echo twig_escape_filter($this->env, $this->getAttribute(($context["backward_compatibility"] ?? null), "css_id", array()), "html", null, true);
+            echo "\"";
+        }
+        echo ">
+\t<ul>
+
+\t\t";
+        // line 9
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["languages"] ?? null));
+        foreach ($context['_seq'] as $context["code"] => $context["language"]) {
+            // line 10
+            echo "\t\t\t<li class=\"";
+            echo twig_escape_filter($this->env, $this->getAttribute($context["language"], "css_classes", array()), "html", null, true);
+            echo " wpml-ls-item-legacy-list-vertical\">
+\t\t\t\t<a href=\"";
+            // line 11
+            echo twig_escape_filter($this->env, $this->getAttribute($context["language"], "url", array()), "html", null, true);
+            echo "\"";
+            if ($this->getAttribute($this->getAttribute($context["language"], "backward_compatibility", array()), "css_classes_a", array())) {
+                echo " class=\"";
+                echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute($context["language"], "backward_compatibility", array()), "css_classes_a", array()), "html", null, true);
+                echo "\"";
+            }
+            echo ">";
+            // line 12
+            if ($this->getAttribute($context["language"], "flag_url", array())) {
+                // line 13
+                echo "<img class=\"";
+                echo twig_escape_filter($this->env, ($context["css_classes_flag"] ?? null), "html", null, true);
+                echo "\" src=\"";
+                echo twig_escape_filter($this->env, $this->getAttribute($context["language"], "flag_url", array()), "html", null, true);
+                echo "\" alt=\"";
+                echo twig_escape_filter($this->env, $this->getAttribute($context["language"], "code", array()), "html", null, true);
+                echo "\" title=\"";
+                echo twig_escape_filter($this->env, $this->getAttribute($context["language"], "flag_title", array()), "html", null, true);
+                echo "\">";
+            }
+            // line 16
+            if (($this->getAttribute($context["language"], "is_current", array()) && ($this->getAttribute($context["language"], "native_name", array()) || $this->getAttribute($context["language"], "display_name", array())))) {
+                // line 18
+                $context["current_language_name"] = (($this->getAttribute($context["language"], "native_name", array(), "any", true, true)) ? (_twig_default_filter($this->getAttribute($context["language"], "native_name", array()), $this->getAttribute($context["language"], "display_name", array()))) : ($this->getAttribute($context["language"], "display_name", array())));
+                // line 19
+                echo "<span class=\"";
+                echo twig_escape_filter($this->env, ($context["css_classes_native"] ?? null), "html", null, true);
+                echo "\">";
+                echo twig_escape_filter($this->env, ($context["current_language_name"] ?? null), "html", null, true);
+                echo "</span>";
+            } else {
+                // line 23
+                if ($this->getAttribute($context["language"], "native_name", array())) {
+                    // line 24
+                    echo "<span class=\"";
+                    echo twig_escape_filter($this->env, ($context["css_classes_native"] ?? null), "html", null, true);
+                    echo "\">";
+                    echo twig_escape_filter($this->env, $this->getAttribute($context["language"], "native_name", array()), "html", null, true);
+                    echo "</span>";
+                }
+                // line 27
+                if ($this->getAttribute($context["language"], "display_name", array())) {
+                    // line 28
+                    echo "<span class=\"";
+                    echo twig_escape_filter($this->env, ($context["css_classes_display"] ?? null), "html", null, true);
+                    echo "\">";
+                    // line 29
+                    if ($this->getAttribute($context["language"], "native_name", array())) {
+                        echo "<span class=\"";
+                        echo twig_escape_filter($this->env, ($context["css_classes_bracket"] ?? null), "html", null, true);
+                        echo "\"> (</span>";
+                    }
+                    // line 30
+                    echo twig_escape_filter($this->env, $this->getAttribute($context["language"], "display_name", array()), "html", null, true);
+                    // line 31
+                    if ($this->getAttribute($context["language"], "native_name", array())) {
+                        echo "<span class=\"";
+                        echo twig_escape_filter($this->env, ($context["css_classes_bracket"] ?? null), "html", null, true);
+                        echo "\">)</span>";
+                    }
+                    // line 32
+                    echo "</span>";
+                }
+            }
+            // line 36
+            echo "</a>
+\t\t\t</li>
+\t\t";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['code'], $context['language'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 39
+        echo "
+\t</ul>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "template.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  125 => 39,  117 => 36,  113 => 32,  107 => 31,  105 => 30,  99 => 29,  95 => 28,  93 => 27,  86 => 24,  84 => 23,  77 => 19,  75 => 18,  73 => 16,  62 => 13,  60 => 12,  51 => 11,  46 => 10,  42 => 9,  30 => 6,  27 => 5,  25 => 4,  23 => 3,  21 => 2,  19 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "template.twig", "D:\\laragon\\www\\vpWithWpml1\\wp-content\\plugins\\sitepress-multilingual-cms\\templates\\language-switchers\\legacy-list-vertical\\template.twig");
+    }
+}
